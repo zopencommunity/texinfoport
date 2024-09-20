0ce9460607224499dd5ca60b75272da51965f7ab
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:52 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/texinfoport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/texinfoport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/texinfoport.git'), string(name: 'PORT_DESCRIPTION', value: 'Texinfo	is the official	documentation format of	the GNU	project. ' ), string(name: 'NODE_LABEL', value: "v2r4")]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/texinfoport.git'), string(name: 'PORT_DESCRIPTION', value: 'Texinfo	is the official	documentation format of	the GNU	project. ' ), string(name: 'NODE_LABEL', value: "v2r4")]
         }
 }
